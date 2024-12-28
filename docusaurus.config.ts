64385a35f8eec8a38e9525f8a3dc249e4107ac71
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2024-12-28 16:13:40 +0800

    ci: deploy-action
    * feat: change favicon and title
    
    * docs: change yarn to pnpm
    
    * build: deploy settings
    
    * ci: test-deploy and deploy
    
    * ci: change node version
    
    * ci: install pnpm

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,20 +5,22 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Message-exp',
+  tagline: 'A messaging software dev team',
+  favicon: 'img/favicon_msg_exp.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://message-exp.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'message-exp', // Usually your GitHub org/user name.
+  projectName: 'message-exp.github.io', // Usually your repo name.
+  
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -40,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/message-exp/docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
