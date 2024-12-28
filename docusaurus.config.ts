1e74ef2472ced6935fe105673524abf372a58bed
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2024-12-28 17:09:07 +0800

    docs: change title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Message-exp',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
